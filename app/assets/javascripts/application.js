19a3e21650334aa976cf75f1985ff5d67677cc36
zhou <liuzhouwork@gmail.com>
2015-02-20 21:37:40 +0800

    added markdown,modified css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+//textarea支持tab缩进
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,14 @@ module ApplicationHelper
       return "#{page_title} · #{base_title}"
     end
   end
+
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                       no_intra_emphasis:true,
+                                       fenced_code_blocks:true,
+                                       disable_indented_code_blocks:true)
+    return markdown.render(text).html_safe
+  end
+
+
 end
